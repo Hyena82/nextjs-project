ef20478a996385476f94504c704ad371fb1c9635
Nguyen Duc Tu <nguyenductu1602@gmail.com>
2024-05-29 10:27:36 +0700

    Inital commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: red;
+`;
+
+const HomePage = () => {
+  return (
+    <Wrapper>
+      <p className="flex">coi nek</p>
+    </Wrapper>
+  );
+};
+
+export default HomePage;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,12 @@
+"use client";
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  .flex{
+    display: flex;
+    background-color: green;
+  }
+`;
+
+export default GlobalStyles;
