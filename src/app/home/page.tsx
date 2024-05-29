954e6054e01abb98831a388653f660dbb372bf26
Nguyen Duc Tu <nguyenductu1602@gmail.com>
2024-05-29 10:40:04 +0700

    add Box, Flex uikit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,19 @@
 "use client";
+import { Box } from "@/components/Box";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  background-color: red;
+  width: 100%;
+  height: 100vh;
 `;
 
 const HomePage = () => {
   return (
     <Wrapper>
       <p className="flex">coi nek</p>
+      <Box p={4} className="hi">
+        chao moi nguoi toi la box
+      </Box>
     </Wrapper>
   );
 };
